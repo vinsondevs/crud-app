b9f3e20a08ec841ab183a86b387a87c557172986
vinsondevs <rishidevs@hotmail.com>
2019-01-23 07:45:24 +0530

    this is upto delete operation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -14,23 +14,16 @@ export class RegistrationComponent  {
     submitType: string = 'save';
     selectedRow: number;
     countries: string[] = ['US', 'UK', 'India', 'UAE'];
-    onNew() {
-    // Initiate new registration.
-    this.regModel = new Registration();
-    // Change submitType to 'Save'.
-    this.submitType = 'Save';
-    // display registration entry section.
-    this.showNew = true;
-    } 
+    
   constructor(
     public firstName: string ="",
     public lastName: string = "",
     public dob: NgbDateStruct = null,
     public email: string = "",
     public password: string = "",
-    public counry: string = "select country"
+    public country: string = "select country"
   ) { 
-    this.registrations.push(new Registraction(
+    this.registrations.push(new Registration(
       'Johan',
       'pete',
       {year: 1980, month: 5, day: 12 },
@@ -40,6 +33,36 @@ export class RegistrationComponent  {
       this.registrations.push(new Registration('Mohamed', 'Tariq', {year: 1975, month: 12, day: 3}, 'tariq@gmail.com', 'tariq123', 'UAE'));
       this.registrations.push(new Registration('Nirmal', 'Kumar', {year: 1970, month: 7, day: 25}, 'nirmal@gmail.com', 'nirmal123', 'India'));
   }
-
-
+  onNew() {
+    // Initiate new registration.
+    this.regModel = new Registration();
+    // Change submitType to 'Save'.
+    this.submitType = 'Save';
+    // display registration entry section.
+    this.showNew = true;
+    } 
+    onSave(){
+      if(this.submitType === 'save'){
+        this.registrations.push(this.regModel);
+      }
+      else{
+        this.registrations[this.selectedRow].firstName = this.regModel.firstName;
+        this.registrations[this.selectedRow].lastName = this.regModel.lastName;
+        this.registrations[this.selectedRow].dob = this.regModel.dob;
+        this.registrations[this.selectedRow].email = this.regModel.email;
+        this.registrations[this.selectedRow].password = this.regModel.password;
+        this.registrations[this.selectedRow].country = this.regModel.country;
+      }
+      this.showNew = false;
+    }
+    onEdit(index: number){
+      this.selectedRow = index;
+      this.regModel = new Registration();
+      this.regModel = Object.assign({}, this.registrations[this.selectedRow]);
+      this.submitType = 'update';
+      this.showNew = true;
+    }
+    onDelete(index: number){
+      this.registrations.splice(index, 1);
+    }
 }
